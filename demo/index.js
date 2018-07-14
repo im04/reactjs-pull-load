77c004d3a54c92cba36e9850cadda3d5c902edc9
guo <921198047@qq.com>
2018-07-14 13:40:00 +0800

    解除忽略dist目录

--- demo/index.js
+++ demo/index.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDom from 'react-dom';
-import PullLoad, {STATS} from "../dist/index";
+import PullLoad, {STATS} from 'reactjs-pull-load';
 console.log(<PullLoad/>, STATS);
 console.log(root);
 ReactDom.render(<div>
